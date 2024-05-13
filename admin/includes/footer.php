1b0f74d3fe4e50bdd95ef8c98e1dfa48277e45a6
alphoncesteven <alphoncesteven@gmail.com>
2024-05-14 00:49:16 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>Credit:<a href="https://www.campcodes.com">CampCodes</a></b>
+  </div>
+  <strong>UAUTS'o &copy; 2024 | Configured by AfsaTech </strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>Credit:<a href="https://www.campcodes.com">CampCodes</a></b>
+    </div>
+    <strong>UAUTS'o &copy; 2024 | Configured by AfsaTech </strong>
+  </div>
+  <!-- /.container -->
+</footer>
