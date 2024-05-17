c19bb81e469a8ed563930273b9cefa58536c67f8
alphoncesteven <alphoncesteven@gmail.com>
2024-05-17 08:59:22 +0300

    Hide reult scripts

--- results.php
+++ results.php
@@ -35,80 +35,93 @@
 
 				<!-- Main content -->
 				<section class="content">
+
 					<?php
+					$endtime = strtotime("2024-05-17");
+					$currenttime = time();
 					$parse = parse_ini_file('admin/config.ini', FALSE, INI_SCANNER_RAW);
 					$title = $parse['election_title'];
 					?>
 					<h1 class="page-header text-center title"><b><?php echo strtoupper($title); ?></b></h1>
 
 					<div class="row">
-
-						<?php
-						$sql = "SELECT * FROM positions ORDER BY priority ASC";
-						$query = $conn->query($sql);
-
-						while ($row = $query->fetch_assoc()) {
-							$position_name = $row['description']; // Assuming the position name field in your database is 'position_name'
-							echo '<div style="border:1px; margin-top:10px;" class="col-md-12 mb-4">';
-							echo '<h2>' . $position_name . '</h2>'; // Display the position name
-							$sql = "SELECT * FROM candidates WHERE position_id='" . $row['id'] . "'";
-							$cquery = $conn->query($sql);
-							while ($crow = $cquery->fetch_assoc()) {
-
+						<?php if ($currenttime >= $endtime) {
 						?>
-								<div style="border:1px solid blue; border-radius:9px; margin-top:10px;" class="row shadow-md p-2  rounded shadow-md ">
-									<div class="col-md-3">
-										<img style="border-radius: 50%;height:90px;" class="rounded-pill max-130 p-2" src="images/profile.jpg" alt="">
-									</div>
-									<div class="col-md-9 align-self-center">
-										<h4 class="mt-3 fs-5 mb-1 fw-bold"><?php echo $crow['firstname'] . ' ' . $crow['lastname']; ?></h4> <!-- Assuming candidate name field in your database is 'candidate_name' -->
-										<?php
+							<div class="text-center">
+								<h3 style="color: red;margin-top:100px;">WELCOME TO UAUTSO NIGTH AWARDS</h3>
+								<!-- <a href="#view" data-toggle="modal" class="btn btn-flat btn-primary btn-lg">View Ballot</a> -->
+							</div>
+						<?php
+						} else {
+						?>
 
-										$sql = "SELECT COUNT(*) AS count_votes FROM votes WHERE candidate_id='" . $crow['id'] . "'";
-										$vote_query = $conn->query($sql);
+							<?php
+							$sql = "SELECT * FROM positions ORDER BY priority ASC";
+							$query = $conn->query($sql);
 
-										if ($vote_query->num_rows > 0) {
+							while ($row = $query->fetch_assoc()) {
+								$position_name = $row['description']; // Assuming the position name field in your database is 'position_name'
+								echo '<div style="border:1px; margin-top:10px;" class="col-md-12 mb-4">';
+								echo '<h2>' . $position_name . '</h2>'; // Display the position name
+								$sql = "SELECT * FROM candidates WHERE position_id='" . $row['id'] . "'";
+								$cquery = $conn->query($sql);
+								while ($crow = $cquery->fetch_assoc()) {
 
-											$votes = $vote_query->fetch_assoc();
+							?>
+									<div style="border:1px solid blue; border-radius:9px; margin-top:10px;" class="row shadow-md p-2  rounded shadow-md ">
+										<div class="col-md-3">
+											<img style="border-radius: 50%;height:90px;" class="rounded-pill max-130 p-2" src="images/profile.jpg" alt="">
+										</div>
+										<div class="col-md-9 align-self-center">
+											<h4 class="mt-3 fs-5 mb-1 fw-bold"><?php echo $crow['firstname'] . ' ' . $crow['lastname']; ?></h4>
+											<!-- Assuming candidate name field in your database is 'candidate_name' -->
 
-											$candidate_votes = $votes['count_votes'];
+											<?php
 
-											$sql = "SELECT COUNT(*) AS total_votes FROM votes WHERE position_id='" . $row['id'] . "'";
-											$tv_query = $conn->query($sql);
+											$sql = "SELECT COUNT(*) AS count_votes FROM votes WHERE candidate_id='" . $crow['id'] . "'";
+											$vote_query = $conn->query($sql);
 
-											if ($tv_query->num_rows > 0) {
+											if ($vote_query->num_rows > 0) {
 
-												$total_votes = $tv_query->fetch_assoc();
+												$votes = $vote_query->fetch_assoc();
 
-												// die($total_votes['total_votes']);
+												$candidate_votes = $votes['count_votes'];
 
-												if ($total_votes['total_votes'] != 0) {
+												$sql = "SELECT COUNT(*) AS total_votes FROM votes WHERE position_id='" . $row['id'] . "'";
+												$tv_query = $conn->query($sql);
 
-													$avarage = number_format(($candidate_votes /	$total_votes['total_votes']) * 100, 0, '.', '');
-												} else {
+												if ($tv_query->num_rows > 0) {
 
-													$avarage = 0;
+													$total_votes = $tv_query->fetch_assoc();
+
+													// die($total_votes['total_votes']);
+
+													if ($total_votes['total_votes'] != 0) {
+
+														$avarage = number_format(($candidate_votes /	$total_votes['total_votes']) * 100, 0, '.', '');
+													} else {
+
+														$avarage = 0;
+													}
+
+													// echo $total_votes['total_votes'];
+													echo '<p style="font-size:14px;" class="h5 fs-8 mb-2 fw-bold">Votes :' .  $candidate_votes . ' </p>';
+													echo '<div class="progress-bar p-7 bg-warning" role="progressbar" aria-label="Example with label" style="width:' . $avarage . '%;" aria-valuenow="' . $avarage . '" aria-valuemin="0" aria-valuemax="100">' . $avarage . '%</div>';
 												}
-
-
-
-
-												// echo $total_votes['total_votes'];
-												echo '<p style="font-size:14px;" class="h5 fs-8 mb-2 fw-bold">Votes :' .  $candidate_votes . ' </p>';
-												echo '<div class="progress-bar p-7 bg-warning" role="progressbar" aria-label="Example with label" style="width:' . $avarage . '%;" aria-valuenow="' . $avarage . '" aria-valuemin="0" aria-valuemax="100">' . $avarage . '%</div>';
 											}
-										}
 
-										?>
+											?>
 
 
+										</div>
 									</div>
-								</div>
-						<?php
+							<?php
+								}
+								echo '</div>';
 							}
-							echo '</div>';
-						}
-						?>
+							?>
+
+						<?php } ?>
 
 
 					</div>
