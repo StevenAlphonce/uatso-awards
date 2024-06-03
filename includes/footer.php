dad1159731459c2055f98a313f1054509555ad09
alphoncesteven <alphoncesteven@gmail.com>
2024-06-03 15:06:57 +0300

    Correction on footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <footer class="main-footer">
   <div class="container">
     <div class="pull-right hidden-xs">
-      <b>Credit:<a href="https://www.campcodes.com">CampCodes</a></b>
+      <b>Developed by Group C</b>
     </div>
-    <strong>UAUTS'o &copy; 2024 | Configured by AfsaTech </strong>
+    <strong>UAUTS'o &copy; 2024 </strong>
   </div>
   <!-- /.container -->
 </footer>
